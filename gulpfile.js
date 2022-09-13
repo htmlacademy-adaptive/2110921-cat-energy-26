c4d78d817e91e8cf78466521c3394a4801c7928d
Nadezhda Strelnikova <nvs.strelnikova@gmail.com>
2022-09-13 14:04:51 +0300

    Подготовка проекта к сборке

--- gulpfile.js
+++ gulpfile.js
@@ -70,13 +70,13 @@ const createWebp = () => {
 //SVG
 
 const svg = () => {
-  gulp.src(['source/img/**/*.svg', '!source/img/sprite-icons/*.svg'])
+  return gulp.src(['source/img/**/*.svg', '!source/img/sprite-icons/*.svg'])
   .pipe(svgo())
   .pipe(gulp.dest('build/img'));
 }
 
 const createSprite = () => {
-  gulp.src('source/img/sprite-icons/*.svg')
+  return gulp.src('source/img/sprite-icons/*.svg')
   .pipe(svgo())
   .pipe(svgstore({
     inlineSvg: true
