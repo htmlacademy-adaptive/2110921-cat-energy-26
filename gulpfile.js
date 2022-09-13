0833488bc3b08f971d7c5e03c3b5aef417cb688d
Nadezhda Strelnikova <nvs.strelnikova@gmail.com>
2022-09-13 13:15:01 +0300

    Подготавливает проект к сборке

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ import terser from 'gulp-terser';
 import squoosh from 'gulp-libsquoosh';
 import svgo from 'gulp-svgmin';
 import svgstore from "gulp-svgstore";
+import {deleteAsync} from 'del';
 
 // Styles
 
@@ -44,18 +45,20 @@ const scripts = () => {
 }
 
 //Images
+
 const optimizeImages = () => {
   return gulp.src('source/img/**/*.{jpg,png}')
   .pipe(squoosh())
   .pipe(gulp.dest('build/img'));
 }
 
-export const copyImages = () => {
+const copyImages = () => {
   return gulp.src('source/img/**/*.{jpg,png}')
   .pipe(gulp.dest('build/img'));
 }
 
 //WebP
+
 const createWebp = () => {
   return gulp.src(['source/img/**/*.{jpg,png}', '!source/img/favicons/*'])
   .pipe(squoosh({
@@ -65,14 +68,39 @@ const createWebp = () => {
 }
 
 //SVG
+
 const svg = () => {
-  gulp.src(['source/img/*.svg', '!source/img/favicons/*.svg'])
+  gulp.src(['source/img/**/*.svg', '!source/img/sprite-icons/*.svg'])
   .pipe(svgo())
   .pipe(gulp.dest('build/img'));
 }
 
-const sprite = () => {
+const createSprite = () => {
+  gulp.src('source/img/sprite-icons/*.svg')
+  .pipe(svgo())
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename('sprite.svg'))
+  .pipe(gulp.dest('build/img'));
+}
 
+//Copy
+
+const copy = (done) => {
+  return gulp.src([
+    'source/fonts/*.{woff,woff2}',
+    'source/*ico',
+    'source/manifest.webmanifest'
+  ], {
+    base: 'source'
+  })
+  .pipe(gulp.dest('build'));
+}
+
+//Clean
+const clean = () => {
+  return deleteAsync('build');
 }
 
 // Server
@@ -89,14 +117,39 @@ const server = (done) => {
   done();
 }
 
+//Reload
+
+const reload = (done) => {
+  browser.reload();
+  done();
+}
+
 // Watcher
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+  gulp.watch('source/js/*.js', gulp.series(scripts));
   gulp.watch('source/*.html').on('change', browser.reload);
 }
 
+//Build
 
-export default gulp.series(
-  html, styles, server, watcher
+export const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    html,
+    styles,
+    scripts,
+    createWebp,
+    svg,
+    createSprite
+  )
 );
+
+// //Default
+
+// export default gulp.series(
+//   html, styles, server, watcher
+// );
